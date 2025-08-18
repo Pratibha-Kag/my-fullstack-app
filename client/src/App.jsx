373bd68689b4cb79696e57a7dd050915b69ac463
Pratibha-Kag <pratibhakag.s29@gmail.com>
2025-08-18 17:09:43 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello this is React app</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
