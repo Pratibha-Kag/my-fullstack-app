403d5a31611e3d471c44bad7da0d150378929568
Pratibha-Kag <pratibhakag.s29@gmail.com>
2025-08-24 12:55:01 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,57 @@
 const express = require("express");
-const app = express();
-const PORT = 5000;
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UserModel = require("./models/Users");
 
+const app = express();
+app.use(cors());
 app.use(express.json());
 
-let users = [];
+//Connection string
+mongoose.connect("mongodb://127.0.0.1:27017/Crud");
+
+app.get("/", (req, res) => {
+  UserModel.find({})
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+app.get("/getUser/:id", (req, res) => {
+  const id = req.params.id;
+  UserModel.findById({ _id: id })
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+app.put("/updateUser/:id", (req, res) => {
+  const id = req.params.id;
+  UserModel.findByIdAndUpdate(
+    id,
+    { name: req.body.name, email: req.body.email, age: req.body.age },
+    { new: true } // ✅ return updated document
+  )
+    .then((user) => res.json(user))
+    .catch((err) => res.status(500).json(err));
+});
+
+app.delete("/deleteUser/:id", (req, res) => {
+  const id = req.params.id;
+  UserModel.findByIdAndDelete(id)
+    .then((deletedUser) => {
+      if (!deletedUser) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      res.json({ message: "User deleted successfully", deletedUser });
+    })
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+app.post("/createUser", (req, res) => {
+  UserModel.create(req.body)
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
